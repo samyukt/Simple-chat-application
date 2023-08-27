a12052e3e0f13b8d6a0e27db50f4975c8c71ff97
samyukt <samyukt.shah@gmail.com>
2023-08-27 21:01:54 +0200

    Fix the io issue

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIO(server);
 
-// Serve static files (optional, if you have any)
-app.get('/', (req, res) => {
-  app.use(express.static(__dirname + "/public"));
-});
-//app.use(express.static(__dirname + '/public'));
-// Listen for socket connections
+const PORT = 3000;
+
+// Serve static files (optional)
+app.use(express.static(__dirname + '/public'));
+
+// Set up connection event
 io.on('connection', (socket) => {
   console.log('A user connected');
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-  // Listen for incoming messages
-  socket.on('chat message', (msg) => {
+
+  // Listen for incoming message event
+  socket.on('message', (data) => {
+    console.log('Received message:', data);
+    
     // Broadcast the message to all connected clients
-    io.emit('chat message', msg);
+    io.emit('message', data);
   });
 
   // Handle disconnection
-
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
 
-
-const PORT = 3000;
+// Start the server
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:3000/ ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
